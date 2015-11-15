128199143bde19b5d6a00555ab6e44d0ae0ab9df
Min Si <msi@il.is.s.u-tokyo.ac.jp>
2015-11-15 00:06:36 -0600

    Removed op from adapt fence/win_set_info overhead benchmarks.

--- test/perf/adapt/overhead_win_setinfo.c
+++ test/perf/adapt/overhead_win_setinfo.c
@@ -11,36 +11,17 @@
 #include <cperf.h>
 
 /* This benchmark measures the overhead of win_set_info(symmetric) with different
- * adaptation modes.
- *
- * Note that to avoid performance difference caused by adaptation, this test
- * should not add computing delay, and should use only 1 user process and 1 ghost
- * process on every node to avoid work overload issue.*/
+ * adaptation modes. */
 
 #define SKIP 100
-#define ITER 1000
+#define ITER 10000
+#define WIN_SIZE 1024
+
 int rank, nprocs;
 double *winbuf, *locbuf = NULL;
 MPI_Win win;
-int size = 16;
 char async_sched_level_str[CPERF_ENVVAL_MAXLEN] = { 0 };
 char cperf_info_async_config[CPERF_ENVVAL_MAXLEN] = { 0 };
-
-static void issue_ops(MPI_Info async_info)
-{
-    int i, dst;
-    MPI_Win_set_info(win, async_info);
-    MPI_Win_lock_all(0, win);
-
-    for (dst = 0; dst < nprocs; dst++) {
-        for (i = 1; i < size; i++) {
-            MPI_Accumulate(&locbuf[i], 1, MPI_DOUBLE, dst, rank, 1, MPI_DOUBLE, MPI_SUM, win);
-        }
-    }
-
-    MPI_Win_unlock_all(win);
-    MPI_Barrier(MPI_COMM_WORLD);
-}
 
 static void run_test(const char *config_val)
 {
@@ -55,12 +36,12 @@ static void run_test(const char *config_val)
     }
 
     for (x = 0; x < SKIP; x++) {
-        issue_ops(async_info);
+        MPI_Win_set_info(win, async_info);
     }
 
     t0 = MPI_Wtime();
     for (x = 0; x < ITER; x++) {
-        issue_ops(async_info);
+        MPI_Win_set_info(win, async_info);
     }
     t1 = MPI_Wtime();
     t_setinfo = (t1 - t0) / ITER;
@@ -92,14 +73,6 @@ int main(int argc, char *argv[])
         goto exit;
     }
 
-    if (argc > 1)
-        size = atoi(argv[1]);
-
-    if (size <= 0) {
-        fprintf(stderr, "wrong size %d\n", size);
-        goto exit;
-    }
-
     CTEST_perf_read_env("CSP_ASYNC_SCHED_LEVEL", "per-win", &async_sched_level_str);
     CTEST_perf_read_env("CPERF_INFO_ASYNC_CONFIG", "on", &cperf_info_async_config);
 
@@ -107,11 +80,11 @@ int main(int argc, char *argv[])
     MPI_Info_set(win_info, (char *) "epoch_type", "lockall");
 
     /* size in byte */
-    MPI_Win_allocate(sizeof(double) * size, sizeof(double), MPI_INFO_NULL,
+    MPI_Win_allocate(sizeof(double) * WIN_SIZE, sizeof(double), MPI_INFO_NULL,
                      MPI_COMM_WORLD, &winbuf, &win);
 
-    locbuf = malloc(sizeof(double) * size);
-    memset(locbuf, 0, sizeof(double) * size);
+    locbuf = malloc(sizeof(double) * WIN_SIZE);
+    memset(locbuf, 0, sizeof(double) * WIN_SIZE);
 
     MPI_Barrier(MPI_COMM_WORLD);
     run_test(cperf_info_async_config);
