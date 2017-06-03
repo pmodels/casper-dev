4174b6c7bef96cc656e367561021d4dd9033c409
Min Si <msi@anl.gov>
2017-06-02 20:37:41 -0500

    Added asynchronous progress for pt2pt.
    This commit contains the following basic components:
    1) lock-free queue offloading framework
    2) communicator wrapping
    3) predefined datatype registration
    4) shared window based buffer creation and address translation.
    
    Note that following features are not supported yet:
    1) environment variable to adjust queue size
    2) info based offloading control
    3) wildcard support
    4) better support shared ghosts. This code contains a basic tag-based
    workaround.
    5) derived datatype support
    6) pool based pending queue allocation.

--- src/common/include/Makefile.mk
+++ src/common/include/Makefile.mk
@@ -8,4 +8,6 @@ libcasper_la_SOURCES += src/common/include/csp.h \
 			src/common/include/csp_mlock.h \
 			src/common/include/csp_msg.h \
 			src/common/include/csp_thread.h \
-			src/common/include/csp_util.h
+			src/common/include/csp_util.h   \
+			src/common/include/csp_offload.h \
+			src/common/include/csp_datatype.h
