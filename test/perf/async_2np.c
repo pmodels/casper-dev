8b9ce7f975a9e5c4054f0de78d678650e6326d11
Min Si <msi@anl.gov>
2017-11-01 11:49:52 -0500

    Test: added contig/noncontig, fop/cas options in async_2np.

--- test/perf/async_2np.c
+++ test/perf/async_2np.c
@@ -13,12 +13,14 @@
  * Rank 0 performs lockall-accumulate-flush-unlockall, and rank 1 performs
  * compute(busy wait)-test(poll MPI progress).*/
 
-#define SIZE 4
 #define SLEEP_TIME 100  //us
 
 //#define DEBUG
 #define CHECK
 #define ITER 10000
+#define SUB_DLEN 2
+#define DLEN 4
+#define SIZE (DLEN*DLEN*DLEN)
 
 #ifdef DEBUG
 #define debug_printf(str,...) {fprintf(stdout, str, ## __VA_ARGS__);fflush(stdout);}
@@ -26,22 +28,32 @@
 #define debug_printf(str,...) {}
 #endif
 
-#ifdef ENABLE_CSP
-#include <casper.h>
-int CSP_NUM_G = 1;
-#endif
+#define MPI_DATATYPE MPI_DOUBLE
+#define DATATYPE double
 
 MPI_Win win;
 double *winbuf, locbuf[SIZE];
+int locbuf_int[SIZE], resbuf_int[1], compbuf_int[1];    /* used for FOP or CAS. */
 int rank, nprocs;
 int NOP = 1;
+MPI_Datatype target_type = MPI_DATATYPE;
 
-const char *OP_TYPE_NM[3] = { "ACC", "PUT", "GET" };
+#ifdef TEST_RMA_CONTIG
+const char *dt_str = "contig";
+#else
+const char *dt_str = "subarray";
+#endif
+
 enum {
     OP_ACC,
     OP_PUT,
-    OP_GET
+    OP_GET,
+    OP_FOP,
+    OP_CAS,
+    OP_MAX
 };
+const char *OP_TYPE_NM[OP_MAX] = { "ACC", "PUT", "GET", "FOP", "CAS" };
+
 int OP_TYPE = OP_ACC;
 
 static void usleep_by_count(unsigned long us)
@@ -49,6 +61,34 @@ static void usleep_by_count(unsigned long us)
     double start = MPI_Wtime() * 1000 * 1000;
     while (MPI_Wtime() * 1000 * 1000 - start < us);
     return;
+}
+
+static void create_datatype(void)
+{
+#if defined(TEST_RMA_CONTIG)
+    target_type = MPI_DATATYPE;
+#else
+    int sizes[3], subsizes[3], starts[3];
+    MPI_Aint lb = 0;
+
+    sizes[0] = DLEN;
+    sizes[1] = DLEN;
+    sizes[2] = DLEN;
+    subsizes[0] = SUB_DLEN;
+    subsizes[1] = SUB_DLEN;
+    subsizes[2] = SUB_DLEN;
+    starts[0] = starts[1] = starts[2] = 0;
+
+    MPI_Type_create_subarray(3, sizes, subsizes, starts, MPI_ORDER_C, MPI_DATATYPE, &target_type);
+    MPI_Type_commit(&target_type);
+#endif
+}
+
+static void release_datatype(void)
+{
+#if !defined(TEST_RMA_CONTIG)
+    MPI_Type_free(&target_type);
+#endif
 }
 
 static int run_test(int time)
@@ -60,6 +100,7 @@ static int run_test(int time)
     MPI_Status status;
     int buf[1];
     int flag = 0;
+    int origin_count = 0, target_count = 0;
 
     if (rank == 0) {
         dst = 1;
@@ -72,26 +113,47 @@ static int run_test(int time)
         MPI_Irecv(buf, 1, MPI_INT, src, 0, MPI_COMM_WORLD, &request);
     }
 
+#if defined(TEST_RMA_CONTIG)
+    origin_count = 1;
+    target_count = 1;
+#else
+    origin_count = SUB_DLEN * SUB_DLEN * SUB_DLEN;
+    target_count = 1;
+#endif
     t0 = MPI_Wtime();
     for (x = 0; x < ITER; x++) {
 
         // rank 0 does RMA communication
         if (rank == 0) {
-            switch(OP_TYPE) {
+            switch (OP_TYPE) {
             case OP_PUT:
                 for (i = 0; i < NOP; i++)
-                    MPI_Put(&locbuf[0], 1, MPI_DOUBLE, dst, 0, 1, MPI_DOUBLE, win);
+                    MPI_Put(locbuf, origin_count, MPI_DATATYPE, dst, 0, target_count, target_type,
+                            win);
                 MPI_Win_flush_all(win);
                 break;
             case OP_GET:
                 for (i = 0; i < NOP; i++)
-                    MPI_Get(&locbuf[0], 1, MPI_DOUBLE, dst, 0, 1, MPI_DOUBLE, win);
+                    MPI_Get(locbuf, origin_count, MPI_DATATYPE, dst, 0, target_count, target_type,
+                            win);
+                MPI_Win_flush_all(win);
+                break;
+            case OP_FOP:
+                for (i = 0; i < NOP; i++)
+                    MPI_Fetch_and_op(&locbuf_int[0], resbuf_int, MPI_INT, dst, 0, MPI_SUM, win);
+                MPI_Win_flush_all(win);
+                break;
+            case OP_CAS:
+                for (i = 0; i < NOP; i++)
+                    MPI_Compare_and_swap(&locbuf_int[0], compbuf_int, resbuf_int, MPI_INT, dst, 0,
+                                         win);
                 MPI_Win_flush_all(win);
                 break;
             case OP_ACC:
             default:
                 for (i = 0; i < NOP; i++)
-                    MPI_Accumulate(&locbuf[0], 1, MPI_DOUBLE, dst, 0, 1, MPI_DOUBLE, MPI_SUM, win);
+                    MPI_Accumulate(locbuf, origin_count, MPI_DATATYPE, dst, 0, target_count,
+                                   target_type, MPI_SUM, win);
                 MPI_Win_flush_all(win);
                 break;
             }
@@ -124,12 +186,12 @@ static int run_test(int time)
     if (rank == 0) {
 #ifdef ENABLE_CSP
         fprintf(stdout,
-                "casper: %s, comp_size %d num_op %d nprocs %d total_time %.2lf\n",
-                OP_TYPE_NM[OP_TYPE], time, NOP, nprocs, t_total * 1000 * 1000);
+                "casper: %s, %s, comp_size %d num_op %d nprocs %d total_time %.2lf\n",
+                OP_TYPE_NM[OP_TYPE], dt_str, time, NOP, nprocs, t_total * 1000 * 1000);
 #else
         fprintf(stdout,
-                "orig: %s, comp_size %d num_op %d nprocs %d total_time %.2lf\n",
-                OP_TYPE_NM[OP_TYPE], time, NOP, nprocs, t_total * 1000 * 1000);
+                "orig: %s, %s, comp_size %d num_op %d nprocs %d total_time %.2lf\n",
+                OP_TYPE_NM[OP_TYPE], dt_str, time, NOP, nprocs, t_total * 1000 * 1000);
 #endif
     }
 
@@ -157,17 +219,17 @@ int main(int argc, char *argv[])
     if (argc >= 6) {
         OP_TYPE = atoi(argv[5]);
     }
-    if ((OP_TYPE != OP_ACC) && (OP_TYPE != OP_PUT) && (OP_TYPE != OP_GET)) {
+
+#if !defined(TEST_RMA_CONTIG)
+    if (OP_TYPE == OP_FOP || OP_TYPE == OP_CAS) {
         if (rank == 0)
-            fprintf(stderr, "Wrong op type %d\n", OP_TYPE);
-        MPI_Abort(MPI_COMM_WORLD, 1);
+            fprintf(stderr, "Do not support FOP or CAS with derived datatype.\n");
+        goto exit;
     }
+#endif
 
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-#ifdef ENABLE_CSP
-    CSP_ghost_size(&CSP_NUM_G);
-#endif
 
     debug_printf("[%d]comm_size done\n", rank);
 
@@ -185,7 +247,9 @@ int main(int argc, char *argv[])
     MPI_Info_set(win_info, (char *) "epoch_type", (char *) "lockall");
 
     // size in byte
-    MPI_Win_allocate(sizeof(double), sizeof(double), win_info, MPI_COMM_WORLD, &winbuf, &win);
+    MPI_Win_allocate(sizeof(DATATYPE) * SIZE, sizeof(DATATYPE), win_info, MPI_COMM_WORLD,
+                     &winbuf, &win);
+    create_datatype();
 
     /* reset window */
     MPI_Win_lock_all(0, win);
@@ -204,7 +268,7 @@ int main(int argc, char *argv[])
     MPI_Win_free(&win);
 
   exit:
-
+    release_datatype();
     MPI_Finalize();
 
     return 0;
